e91703e53f09a9c3b33e6d2117952b95ca8168d6
Yuriy Lunyov <lunyov.yu@gmail.com>
2023-11-11 14:16:04 +0200

    removed unused import, gitignore clean-up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import OrderEntry from './pages/OrderEntry/OrderEntry';
 import { Container } from 'react-bootstrap';
 import { OrderDetailsCtxProvider } from './contexts/OrderDetailsCtx';
-import OrderSummary from './pages/OrderSummary/OrderSummary';
 
 function App() {
   return (
